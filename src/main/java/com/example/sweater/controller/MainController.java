87e06730338312c541d93c5e34379a8455b6b4bc
dinar92 <gdinar92@gmail.com>
2020-05-10 00:31:43 +0300

    Migrate to Freemarker

--- src/main/java/com/example/sweater/controller/MainController.java
+++ src/main/java/com/example/sweater/controller/MainController.java
@@ -6,6 +6,7 @@ import com.example.sweater.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,9 +26,17 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String main(Map<String, Object> model) {
-        Iterable<Message> all = messageRepo.findAll();
-        model.put("messages", all);
+    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model) {
+        Iterable<Message> all;
+        if (filter != null && !filter.isEmpty()) {
+            all = messageRepo.findByTag(filter);
+        } else {
+            all = messageRepo.findAll();
+        }
+
+        model.addAttribute("messages", all);
+        model.addAttribute("filter", filter);
+
         return "main";
     }
 
@@ -40,19 +49,6 @@ public class MainController {
         Message message = new Message(text, tag, user);
         messageRepo.save(message);
         Iterable<Message> messages = messageRepo.findAll();
-        model.put("messages", messages);
-        return "main";
-    }
-
-    @PostMapping("/filter")
-    public String filter(@RequestParam String filter, Map<String, Object> model) {
-        Iterable<Message> messages;
-
-        if (filter != null && !filter.isEmpty()) {
-            messages = messageRepo.findByTag(filter);
-        } else {
-            messages = messageRepo.findAll();
-        }
         model.put("messages", messages);
         return "main";
     }
